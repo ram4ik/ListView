3af7b4179ac7132632f5537622009c667561d33a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-27 16:27:44 +0200

    init

--- ListView/ContentView.swift
+++ ListView/ContentView.swift
@@ -10,7 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        List(0..<10) { value in
+            HStack {
+                Image(systemName: "person")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 50)
+                VStack {
+                    Text("Person \(value)")
+                        .font(.headline)
+                    Text("USA")
+                        .font(.subheadline)
+                }
+            }
+        }
     }
 }
 
